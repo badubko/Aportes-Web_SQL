d0e2c87144c4fd0b0a024f1c02008920b3f0ffca
badubko <badubko@gmail.com>
2018-11-23 14:33:49 -0300

    Creada nueva vers de archivo index_ap_V1.0.php

--- cop_V1.0.sh
+++ cop_V1.0.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 
-cp create.php index.php read.php /var/www/html
+cp create.php index.php index_ap_V1.0.php read.php /var/www/html
 cp ./css/* /var/www/html/css
 cp ./templates/* /var/www/html/templates
--- index_ap_V1.0.php
+++ index_ap_V1.0.php
@@ -3,7 +3,7 @@
 <?php include "templates/header.php"; ?>
 
 	<ul>
-		<li><a href="create.php"><strong>Create Kakita</strong></a> - add a user</li>
+		<li><a href="create.php"><strong>Create Kakita</strong></a> - Agregar Voluntario</li>
 		<li><a href="read.php"><strong>Read   Kakita2</strong></a> - find a user</li>
 	</ul>
 
