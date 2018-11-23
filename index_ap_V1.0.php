a76eb3c16201e1f5323b4e142e4e8ddb4cdaa7b5
badubko <badubko@gmail.com>
2018-11-23 16:59:23 -0300

    Cambiados titulos

--- index_ap_V1.0.php
+++ index_ap_V1.0.php
@@ -3,8 +3,8 @@
 <?php include "templates/header.php"; ?>
 
 	<ul>
-		<li><a href="create_ap_V1.0.php"><strong>Create Kakita</strong></a> - Agregar Voluntario</li>
-		<li><a href="read.php"><strong>Read   Kakita2</strong></a> - find a user</li>
+		<li><a href="create_ap_V1.0.php"><strong>Create VOL</strong></a> - Agregar Voluntario</li>
+		<li><a href="read.php"><strong>Buscar VOL</strong></a> - Buscar Voluntario</li>
 	</ul>
 
 <?php include "templates/footer.php"; ?>
