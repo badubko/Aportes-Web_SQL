013465a58aec151bb5cd07e98ffe6c3d64b3c3a0
badubko <badubko@gmail.com>
2018-11-23 15:40:51 -0300

    Modificado para agregar nvo voluntario

--- create_ap_V1.0.php
+++ create_ap_V1.0.php
@@ -6,17 +6,18 @@
  *
  */
 if (isset($_POST['submit'])) {
-    require "../config_ap_V1.0.php";
-    require "../common_ap_V1.0.php";
+    require "./config_ap_V1.0.php";
+    require "./common_ap_V1.0.php";
     try  {
         $connection = new PDO($dsn, $username, $password, $options);
         
         $new_user = array(
-            "firstname" => $_POST['firstname'],
-            "lastname"  => $_POST['lastname'],
-            "email"     => $_POST['email'],
-            "age"       => $_POST['age'],
-            "location"  => $_POST['location']
+            "nombre" => $_POST['nombres'],
+            "apellido"  => $_POST['apellido'],
+            "dni"     => $_POST['dni'],
+            "profesion"       => $_POST['profesion'],
+            "email_1"  => $_POST['email_1'],
+            "email_2"  => $_POST['email_2']
         );
         $sql = sprintf(
                 "INSERT INTO %s (%s) values (%s)",
@@ -36,49 +37,10 @@ if (isset($_POST['submit'])) {
 <?php require "templates/header.php"; ?>
 
 <?php if (isset($_POST['submit']) && $statement) { ?>
-    <blockquote><?php echo $_POST['firstname']; ?> successfully added.</blockquote>
+    <blockquote><?php echo $_POST['apellido']; ?> successfully added.</blockquote>
 <?php } ?>
 
-<h2>Add a user</h2>
-
-<form method="post">
-    <label for="firstname">First Name</label>
-    <input type="text" name="firstname" id="firstname">
-    <label for="lastname">Last Name</label>
-    <input type="text" name="lastname" id="lastname">
-    <label for="email">Email Address</label>
-    <input type="text" name="email" id="email">
-    <label for="age">Age</label>
-    <input type="text" name="age" id="age">
-    <label for="location">Location</label>
-    <input type="text" name="location" id="location">
-    <input type="submit" name="submit" value="Submit">
-</form>
-
-<a href="index.php">Back to home</a>
-
-<?php require "templates/footer.php"; ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php include "templates/header.php"; ?><h2>Agregar Voluntario V1.0</h2>
+<h2>Agregar Voluntario V1.0</h2>
 
 <form method="post">
 	<label for="nombres">Nombres</label>
@@ -98,4 +60,6 @@ if (isset($_POST['submit'])) {
 
 <a href="index_ap_V1.0.php">Back to home</a>
 
-<?php include "templates/footer.php"; ?>
+<?php require "templates/footer.php"; ?>
+
+
