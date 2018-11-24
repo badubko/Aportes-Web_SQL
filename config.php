ac24424a70c29572f815f08ef6cc9bd799116462
badubko <badubko@gmail.com>
2018-11-23 21:00:44 -0300

    Nuevas pruebas

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Configuration for database connection
+ *
+ */
+
+$host       = "192.168.1.29";
+$username   = "root";
+$password   = "combet_0975?";
+$dbname     = "test"; // will use later
+$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
