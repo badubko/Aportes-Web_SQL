e3ee452a6024fc02c9a2ba3d4d48c005b6d7f2cf
badubko <badubko@gmail.com>
2018-11-24 09:31:46 -0300

    Mas pruebas para lograr que algo ande como esperado

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 $host       = "192.168.1.29";
 $username   = "root";
 $password   = "combet_0975?";
-$dbname     = "test"; // will use later
+$dbname     = "test1"; // will use later
 $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
--- cop_test_V1.0.sh
+++ cop_test_V1.0.sh
@@ -3,8 +3,11 @@ echo
 cp -v -u index.php  /var/www/html
 cp -v -u create.php  /var/www/html
 cp -v -u read.php /var/www/html
+cp -v -u install.php /var/www/html
 
 cp -v -u common.php config.php  /var/www/html
 cp -v -u ./css/* /var/www/html/css
 cp -v -u ./templates/* /var/www/html/templates
+cp -v -u ./data/* /var/www/html/data
+
 echo
