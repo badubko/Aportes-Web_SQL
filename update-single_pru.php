9e5b8f042b2c63aafbbf3dd6292d454f52c0069c
badubko <badubko@gmail.com>
2018-11-26 12:23:42 -0300

    Prueba basica con update-single minimo

---
+++ update-single_pru.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Use an HTML form to edit an entry in the
+ * users table.
+ *
+ */
+
+require "./config.php";
+require "./common.php";
+
+if (isset($_GET['id'])) {
+  echo $_GET['id']; // for testing purposes
+} else {
+    echo "Something went wrong!";
+    echo $_GET['id']; // for testing purposes
+    exit;
+}
+?>
