118c06e35e36cfde72bac2c3d2e8347524c57f9e
badubko <badubko@gmail.com>
2018-11-26 16:43:01 -0300

    Cambio de texto

--- index_ap_V1.2.php
+++ index_ap_V1.2.php
@@ -4,8 +4,8 @@
 
 	<ul>
 		<li><a href="create_ap_V1.2.php"><strong>Create VOL</strong></a> - Agregar Voluntario</li>
-		<li><a href="read_ap_V1.2.php"><strong>Buscar VOL</strong></a> - Buscar Voluntario</li>
-		<li><a href="update_ap_V1.2.php"><strong>Actualizar VOL</strong></a> - edit a user</li>
+		<li><a href="read_ap_V1.2.php"><strong>Buscar VOL</strong></a> - Buscar y modif Voluntario</li>
+		<li><a href="update_ap_V1.2.php"><strong>Actualizar VOL</strong></a> - Mostrar y Modif todos</li>
 	</ul>
 
 <?php include "templates/footer.php"; ?>
