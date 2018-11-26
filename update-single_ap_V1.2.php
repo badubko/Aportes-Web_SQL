142472c96f8760a8d9016d93244d7f5483e97ed0
badubko <badubko@gmail.com>
2018-11-26 01:26:03 -0300

    agregado update

---
+++ update-single_ap_V1.2.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Use an HTML form to edit an entry in the
+ * users table.
+ *
+ */
+require "./config_ap_V1.0.php";
+require "./common.php";
+if (isset($_POST['submit'])) {
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
+    $user =[
+      "dni"      	=> $_POST['dni'],
+      "apellido" 	=> $_POST['apellido'],
+      "nombres"  	=> $_POST['nombres'],
+      "profesion"	=> $_POST['profesion'],
+      "email_1"     => $_POST['email_1'],
+      "email_2"  	=> $_POST['email_2'],
+      "last_update" => $_POST['last_update']
+    ];
+
+    $sql = "UPDATE users 
+            SET dni = :dni, 
+              apellido = :apellido, 
+              nombres = :nombres, 
+              profesion = :profesion,
+              email_1 = :email_1,
+              email_2 = :email_2,  
+              last_update = :last_update             
+            WHERE dni = :dni";
+  
+  $statement = $connection->prepare($sql);
+  $statement->execute($user);
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+}
+  
+if (isset($_GET['dni'])) {
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
+    $dni = $_GET['dni'];
+    $sql = "SELECT * FROM t_users1 WHERE dni = :dni";
+    $statement = $connection->prepare($sql);
+    $statement->bindValue(':dni', $dni);
+    $statement->execute();
+    
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+} else {
+    echo "Something went wrong!";
+    exit;
+}
+?>
+
+<?php require "templates/header.php"; ?>
+
+<?php if (isset($_POST['submit']) && $statement) : ?>
+	<blockquote><?php echo escape($_POST['apellido']); ?> successfully updated.</blockquote>
+<?php endif; ?>
+
+<h2>Edit a user</h2>
+
+<form method="post">
+    <?php foreach ($user as $key => $value) : ?>
+      <label for="<?php echo $key; ?>"><?php echo ucfirst($key); ?></label>
+	    <input type="text" name="<?php echo $key; ?>" dni="<?php echo $key; ?>" value="<?php echo escape($value); ?>" <?php echo ($key === 'dni' ? 'readonly' : null); ?>>
+    <?php endforeach; ?> 
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<a href="index_ap_V1.0.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
