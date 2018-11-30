565c5b3bb0717799632b8a30cefb3fbc6fb2e935
badubko <badubko@gmail.com>
2018-11-30 20:45:40 -0300

    agregado de programas de ejemplo

--- cop_ap_V1.0.sh
+++ cop_ap_V1.0.sh
@@ -7,8 +7,9 @@ echo
 #cp -v -u common_ap_V1.0.php config_ap_V1.0.php  /var/www/html
 
 
-cp -v -u *.php /var/www/html
+cp -v -u *.php *.sh /var/www/html
 
+cp -v -u ./data/* /var/www/html/data
 cp -v -u ./css/* /var/www/html/css
 cp -v -u ./templates/* /var/www/html/templates
 echo
---
+++ tonto_V1.0.php
@@ -0,0 +1,15 @@
+<html>
+   <body>
+   
+      <?php
+         $d = date("D");
+         
+         if ($d == "Fri")
+            echo "$d Have a nice weekend!"; 
+         
+         else
+            echo "$d <br> Have a nice day!"; 
+      ?>
+   
+   </body>
+</html>
