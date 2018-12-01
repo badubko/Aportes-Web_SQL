8c823fe3e2c22c4fb9922e339b4ac1eeb2a735a3
Basilio Dubko <badubko@gmail.com>
2018-12-01 20:20:11 -0300

    Nueva version para actualizar especialidad de un VOL

---
+++ update_ap_esp_nva_V1.2.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * List all users with a link to edit
+ */
+
+try {
+  require "./config_ap_V1.2.php";
+  require "./common_ap_V1.2.php";
+
+  $connection = new PDO($dsn, $username, $password, $options);
+
+  $sql = "SELECT especialidad FROM t_especialidad_user";
+
+  $statement = $connection->prepare($sql);
+  $statement->execute();
+
+  $result = $statement->fetchAll();
+} catch(PDOException $error) {
+  echo $sql . "<br>" . $error->getMessage();
+}
+?>
+<?php require "templates/header.php"; ?>
+        
+<h2>Actualizar Especialidad de un Vol</h2>
+
+<table>
+  <thead>
+    <tr>
+      <th>DNI</th>
+      <th>Especialidad</th>
+    </tr>
+  </thead>
+    <tbody>
+    <?php foreach ($result as $row) : ?>
+      <tr>
+        <td><?php echo escape($row["dni"]); ?></td>
+        <td><?php echo escape($row["especialidad"]); ?></td>
+        <td><a href="update-espec_ap_V1.2.php?dni=<?php echo escape($row["dni"]); ?>">Edit</a></td>
+      </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
+<a href="index_ap_V1.2.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
