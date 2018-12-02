2a137503329d2bffb46b21c745b5ad204adb6560
Basilio Dubko <badubko@gmail.com>
2018-12-02 00:13:15 -0300

    Version que lista correctamente las especialidades de un VOL

--- update-esp_nva_ap_V1.2.php
+++ update-esp_nva_ap_V1.2.php
@@ -10,7 +10,9 @@ try {
 
   $connection = new PDO($dsn, $username, $password, $options);
   $dni = $_GET['dni'];
-  $sql = "SELECT dni, especialidad FROM t_especialidad_user WHERE dni = :dni" ;
+  $apellido = $_GET['apellido'];
+  $nombres = $_GET['nombres'];
+  $sql = "SELECT dni, especialidad , last_update FROM t_especialidad_user WHERE dni = :dni" ;
 
   $statement = $connection->prepare($sql);
   $statement->bindParam(':dni', $dni, PDO::PARAM_STR);
@@ -24,12 +26,15 @@ try {
 <?php require "templates/header.php"; ?>
         
 <h2>Actualizar Especialidad de un Vol</h2>
+<h3><?php echo escape($apellido) , "  " , escape($nombres); ?></h3>
 
 <table>
+
   <thead>
     <tr>
       <th>DNI</th>
       <th>Especialidad</th>
+      <th>Ultima Act</th>
     </tr>
   </thead>
     <tbody>
@@ -37,7 +42,8 @@ try {
       <tr>
         <td><?php echo escape($row["dni"]); ?></td>
         <td><?php echo escape($row["especialidad"]); ?></td>
-        <td><a href="update-espec_ap_V1.2.php?dni=<?php echo escape($row["dni"]); ?>">Edit</a></td>
+        <td><?php echo escape($row["last_update"]); ?></td>
+        <td><a href="update-espec_ap_V1.2.php?dni=<?php echo escape($row["dni"]); ?>">Delete</a></td>
       </tr>
     <?php endforeach; ?>
     </tbody>
