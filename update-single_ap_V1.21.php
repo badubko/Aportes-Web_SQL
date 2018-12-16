cc8605ad0215062d170e6ca360bb412da646fbcc
Basilio Dubko <badubko@gmail.com>
2018-12-16 02:47:20 -0300

    Muestra Apellido, Nombre

--- update-single_ap_V1.21.php
+++ update-single_ap_V1.21.php
@@ -64,7 +64,10 @@ if (isset($_GET['dni'])) {
 <?php require "templates/header.php"; ?>
 
 <?php if (isset($_POST['submit']) && $statement) : ?>
-	<blockquote><?php echo escape($_POST['apellido']); ?> successfully updated.</blockquote>
+	<blockquote>
+	<?php echo escape($_POST['apellido']) , ", " , escape($_POST['nombres']); ?> Fue actualizada OK. 
+	</blockquote>
+	
 <?php endif; ?>
 
 <?php
