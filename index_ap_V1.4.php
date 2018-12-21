56328b57835fba81caa54eeea865bd39583ecdb8
Basilio Dubko <badubko@gmail.com>
2018-12-20 22:48:17 -0300

    Modificados titulos en el index

--- index_ap_V1.4.php
+++ index_ap_V1.4.php
@@ -3,9 +3,9 @@
 <?php include "templates/header.php"; ?>
 
 	<ul>
-		<li><a href="create_ap_V1.4.php"><strong>Create VOL</strong></a> - Agregar Voluntario</li>
-		<li><a href="read_ap_V1.4.php"><strong>Buscar VOL</strong></a> - Buscar y modif Voluntario</li>
-		<li><a href="update_ap_V1.4.php"><strong>Actualizar VOL</strong></a> - Mostrar y Modif todos</li>
+		<li><a href="create_ap_V1.4.php"><strong>Crear VOL</strong></a> - Agregar Voluntario</li>
+		<li><a href="read_ap_V1.4.php"><strong>Actualizar un VOL</strong></a> - Buscar y modif Voluntario</li>
+		<li><a href="update_ap_V1.4.php"><strong>Actualizar todos los VOL</strong></a> - Mostrar y Modif todos</li>
 	</ul>
 
 <?php include "templates/footer.php"; ?>
