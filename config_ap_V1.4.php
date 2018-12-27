f9c0d0a1c84c05227657783dd63064c2a9e42cd8
Basilio Dubko <badubko@gmail.com>
2018-12-27 19:17:48 -0300

    Cambiada la base a la vers 3.5

--- config_ap_V1.4.php
+++ config_ap_V1.4.php
@@ -10,7 +10,7 @@ $vers        = "V1.4";
 $host       = "localhost";
 $username   = "root";
 $password   = "combet_0975?";
-$dbname     = "aportes_V3_2"; // will use later
+$dbname     = "aportes_V3_5"; // will use later
 $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
