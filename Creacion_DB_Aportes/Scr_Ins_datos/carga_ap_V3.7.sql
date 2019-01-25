c2dce3334ec69be18437728352d78e1a3a04b64a
Basilio Dubko <badubko@gmail.com>
2019-01-25 13:34:49 -0300

    Nueva planilla con usuarios sin dni
    Para procesar luego
    script carga modificado

--- Creacion_DB_Aportes/Scr_Ins_datos/carga_ap_V3.7.sql
+++ Creacion_DB_Aportes/Scr_Ins_datos/carga_ap_V3.7.sql
@@ -9,24 +9,28 @@
 --
 USE aportes_V3_7;
 -- Usuarios reales
-\. ./Creacion_DB_Aportes/Scr_Ins_datos/20190122_users_reales.sql
-
+-- \. ./Creacion_DB_Aportes/Scr_Ins_datos/20190122_users_reales.sql
+\. ./Scr_Ins_datos/20190122_users_reales.sql
 -- Usuarios ficticios
 -- \. ./Creacion_DB_Aportes/Scr_Ins_datos/20181227-2010_t_users1_truch.sql
 
 -- Agrega datos de DCs 
 -- --> Atencion: no todos tienen el verdadero numero de DNI
-\. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_Datos_DC_V1.0.sql
+-- \. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_Datos_DC_V1.0.sql
+\. ./Scr_Ins_datos/20190123_Datos_DC_V1.0.sql
 --
 
 -- Agrega datos  de las OSCs. No hay datos detallados de c/OSC
-\. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_t_osc_base_V1.1.sql
+-- \. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_t_osc_base_V1.1.sql
+\. ./Scr_Ins_datos/20190123_t_osc_base_V1.1.sql
 
 -- Agrega los objetivos de las OSCs
-\. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_osc_obj_V1.2.sql
-
+-- \. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_osc_obj_V1.2.sql
+-- \. ./Scr_Ins_datos/20190123_osc_obj_V1.2.sql
 -- Asocia DCs Titulares con OSCs
-\. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_dc_tit_base_V1.0.sql
+-- \. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_dc_tit_base_V1.0.sql
+-- \. ./Scr_Ins_datos/20190123_dc_tit_base_V1.0.sql
 
 -- Asocia DCs Suplentes con OSCs
-\. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_dc_supl_base_V1.0.sql
+-- \. ./Creacion_DB_Aportes/Scr_Ins_datos/20190123_dc_supl_base_V1.0.sql
+-- \. ./Scr_Ins_datos/20190123_dc_supl_base_V1.0.sql
--- Pruebas/buscar_dc1.sql
+++ Pruebas/buscar_dc1.sql
@@ -3,8 +3,6 @@ SELECT
 FROM
 	t_users1
 WHERE
-	dni IN 	(SELECT  dni
-						FROM    t_osc_logs_dc
-						WHERE   osc_nombre LIKE "Wingu%" AND (osc_rol_dc = "Primario" OR  osc_rol_dc = "Suplente" ) 
+	dni IN 	(SELECT  dni FROM    t_osc_logs_dc WHERE   osc_nombre LIKE "APA%" AND (osc_rol_dc = "Titular" OR  osc_rol_dc = "Suplente" ) 
 				) ORDER by apellido;
 	
