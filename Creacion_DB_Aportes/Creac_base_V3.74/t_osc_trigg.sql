3d8d1211f4a194b5716568081b0ff327a50984ea
Basilio Dubko <badubko@gmail.com>
2019-02-04 20:30:00 -0300

    Generacion trigger para actualizar t_osc_logs_dc

--- Creacion_DB_Aportes/Creac_base_V3.74/t_osc_trigg.sql
+++ Creacion_DB_Aportes/Creac_base_V3.74/t_osc_trigg.sql
@@ -3,44 +3,53 @@
 USE aportes_V3_74;
 
 DELIMITER $$
--- El estado del usuario se cambia en t_logs_estado_user
--- El trigger se encarga de modificarlo en la t_user2, poniendo el ultimo estado
--- del Vol
 
--- Hay que escribir un trigger para el caso de que el VOL pase a estar 
--- ND_Temp o De_Baja el DC1 o DC2 de la OSC pase a ser Lalo o sea el DP
--- Por ahora dni= 000 010
 
--- CREATE TRIGGER after_t_osc_update 
-    -- AFTER UPDATE ON t_osc
--- FOR EACH ROW
--- BEGIN
-	-- IF NEW.osc_dc_tit != OLD.osc_dc_tit
-    -- THEN
-		-- INSERT INTO t_osc_logs_dc 	(osc_nombre, dni, osc_rol_dc,osc_f_cambio)
-        -- VALUES 						(OLD.osc_nombre, NEW.osc_dc_tit, "Titular", NEW.osc_f_titular);
-    -- END IF;    
+CREATE TRIGGER after_t_osc_update 
+    AFTER UPDATE ON t_osc
+FOR EACH ROW
+BEGIN
+	IF NEW.osc_dc_tit != OLD.osc_dc_tit
+    THEN
+        IF OLD.osc_dc_tit = '1'
+        THEN
+			INSERT INTO t_osc_logs_dc 	(osc_nombre, dni, osc_rol_dc,osc_f_cambio,osc_comentarios_dc)
+			VALUES 						(OLD.osc_nombre, NEW.osc_dc_tit, "Titular", NEW.osc_f_titular,"Se asigno DC TITULAR");
+        ELSE
+			INSERT INTO t_osc_logs_dc 	(osc_nombre, dni, osc_rol_dc,osc_f_cambio,osc_comentarios_dc)
+			VALUES 						(OLD.osc_nombre, NEW.osc_dc_tit, "Titular", NEW.osc_f_titular,"Se cambio DC TITULAR");
+		END IF;	
+    END IF;    
    
-    -- IF NEW.osc_dc_supl != OLD.osc_dc_supl
-		-- INSERT INTO `t_osc_logs_dc`	(`osc_nombre`,`dni`,`osc_rol_dc`,`osc_f_cambio`)
-		-- VALUES 	(OLD.osc_nombre, NEW.osc_dc_supl, "Suplente", NEW.osc_f_supl);
-	-- END IF;			
--- END;$$
+    IF NEW.osc_dc_supl != OLD.osc_dc_supl
+    THEN
+       IF OLD.osc_dc_supl = '2'
+       THEN   
+			INSERT INTO `t_osc_logs_dc`	(`osc_nombre`,`dni`,`osc_rol_dc`,`osc_f_cambio`,osc_comentarios_dc)
+			VALUES 	(OLD.osc_nombre, NEW.osc_dc_supl, "Suplente", NEW.osc_f_supl,"Se asigno DC SUPLENTE");
+		ELSE
+			INSERT INTO `t_osc_logs_dc`	(`osc_nombre`,`dni`,`osc_rol_dc`,`osc_f_cambio`,osc_comentarios_dc)
+			VALUES 	(OLD.osc_nombre, NEW.osc_dc_supl, "Suplente", NEW.osc_f_supl,"Se Cambio DC SUPLENTE");
+		END IF;
+	END IF;			
+END;$$
 
 CREATE TRIGGER after_t_osc_insert 
     AFTER INSERT ON t_osc
 FOR EACH ROW
 BEGIN
 -- Solo si se asigno un DC titular verdadero, o sea, !=1. Esto se selecciona por programa
-		-- IF NEW.osc_dc_tit != '1' 
-		-- THEN
-			-- INSERT INTO t_osc_logs_dc 	(osc_nombre, dni, osc_rol_dc,osc_f_cambio)
-			-- VALUES 						(NEW.osc_nombre, NEW.osc_dc_tit, "Titular", NEW.osc_f_titular);
---        END IF;
+-- Esto significa que se creo OSC pero no se asigno DC Titular y/o Suplente (quedan como 1 y 2)
+		IF NEW.osc_dc_tit != '1' 
+		THEN
+			INSERT INTO t_osc_logs_dc 	(osc_nombre, dni, osc_rol_dc,osc_f_cambio,osc_comentarios_dc)
+			VALUES 						(NEW.osc_nombre, NEW.osc_dc_tit, "Titular", NEW.osc_f_titular,"Se asigno DC TITULAR");
+       END IF;
 -- Solo si se asigno un DC suplente verdadero, o sea, !=2   Esto se selecciona por programa      
-        IF NEW.osc_dc_supl != "2" 
-		INSERT INTO t_osc_logs_dc 	(osc_nombre, 	dni, 			osc_rol_dc,	osc_f_cambio)
-		VALUES (NEW.osc_nombre, NEW.osc_dc_supl, "Suplente", NEW.osc_f_supl);
+        IF NEW.osc_dc_supl != '2' 
+        THEN
+			INSERT INTO t_osc_logs_dc 	(osc_nombre, 	dni, 			osc_rol_dc,	osc_f_cambio,osc_comentarios_dc)
+			VALUES (NEW.osc_nombre, NEW.osc_dc_supl, 'Suplente', NEW.osc_f_supl,"Se asigno DC SUPLENTE");
 		END IF;
 END;$$
 DELIMITER ;  
