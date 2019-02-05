b1e218a76232db576fa6a6cdbe74667e622d585f
Basilio Dubko <badubko@gmail.com>
2019-02-04 21:53:43 -0300

    Modificacion del trigger de t_osc_logs_ds
    Para que registre correctamente los 3 cambios de estado

--- Creacion_DB_Aportes/Creac_base_V3.74/t_osc_trigg_V3.74.sql
+++ Creacion_DB_Aportes/Creac_base_V3.74/t_osc_trigg_V3.74.sql
@@ -11,6 +11,7 @@ CREATE TRIGGER after_t_osc_update
     AFTER UPDATE ON t_osc
 FOR EACH ROW
 BEGIN
+-- Faltaria excluir que el nuevo sea "2" Pero eso deberia ser por programa...
 	IF NEW.osc_dc_tit != OLD.osc_dc_tit
     THEN
         IF OLD.osc_dc_tit = '1'
@@ -18,8 +19,14 @@ BEGIN
 			INSERT INTO t_osc_logs_dc 	(osc_nombre, dni, osc_rol_dc,osc_f_cambio,osc_comentarios_dc)
 			VALUES 						(OLD.osc_nombre, NEW.osc_dc_tit, "Titular", NEW.osc_f_titular,"Se asigno DC TITULAR");
         ELSE
-			INSERT INTO t_osc_logs_dc 	(osc_nombre, dni, osc_rol_dc,osc_f_cambio,osc_comentarios_dc)
-			VALUES 						(OLD.osc_nombre, NEW.osc_dc_tit, "Titular", NEW.osc_f_titular,"Se cambio DC TITULAR");
+			IF NEW.osc_dc_tit = 1
+			THEN
+				INSERT INTO t_osc_logs_dc 	(osc_nombre, dni, osc_rol_dc,osc_f_cambio,osc_comentarios_dc)
+				VALUES 						(OLD.osc_nombre, NEW.osc_dc_tit, "No_Asignado", NEW.osc_f_titular,"Se desasigno DC TITULAR");
+			ELSE
+				INSERT INTO t_osc_logs_dc 	(osc_nombre, dni, osc_rol_dc,osc_f_cambio,osc_comentarios_dc)
+				VALUES 						(OLD.osc_nombre, NEW.osc_dc_tit, "Titular", NEW.osc_f_titular,"Se cambio DC TITULAR");
+			END IF;
 		END IF;	
     END IF;    
    
@@ -30,8 +37,14 @@ BEGIN
 			INSERT INTO `t_osc_logs_dc`	(`osc_nombre`,`dni`,`osc_rol_dc`,`osc_f_cambio`,osc_comentarios_dc)
 			VALUES 	(OLD.osc_nombre, NEW.osc_dc_supl, "Suplente", NEW.osc_f_supl,"Se asigno DC SUPLENTE");
 		ELSE
-			INSERT INTO `t_osc_logs_dc`	(`osc_nombre`,`dni`,`osc_rol_dc`,`osc_f_cambio`,osc_comentarios_dc)
-			VALUES 	(OLD.osc_nombre, NEW.osc_dc_supl, "Suplente", NEW.osc_f_supl,"Se Cambio DC SUPLENTE");
+			IF NEW.osc_dc_supl = '2'
+			THEN
+				INSERT INTO `t_osc_logs_dc`	(`osc_nombre`,`dni`,`osc_rol_dc`,`osc_f_cambio`,osc_comentarios_dc)
+				VALUES 	(OLD.osc_nombre, NEW.osc_dc_supl, "No_Asignado", NEW.osc_f_supl,"Se desasigno DC SUPLENTE");
+			ELSE
+				INSERT INTO `t_osc_logs_dc`	(`osc_nombre`,`dni`,`osc_rol_dc`,`osc_f_cambio`,osc_comentarios_dc)
+				VALUES 	(OLD.osc_nombre, NEW.osc_dc_supl, "Suplente", NEW.osc_f_supl,"Se cambio DC SUPLENTE");
+			END IF;
 		END IF;
 	END IF;			
 END;$$
