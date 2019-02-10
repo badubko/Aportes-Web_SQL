8fd9f64095e7facdf7b86cc030cb8dd7b37b1953
Basilio Dubko <badubko@gmail.com>
2019-02-10 20:50:55 -0300

    Correccion de nombres de scripts
    Para reflejar correctamente la estructura

--- OSCs/200_OSCs_V1.4.php
+++ OSCs/200_OSCs_V1.4.php
@@ -1,7 +1,7 @@
 <!doctype html>
 
 <?php 
-include "../templates/header_osc_V1.4.php"; 
+include "../templates/header_osc.php"; 
 require "../config_ap_V1.4.php";
 require "../common_ap_V1.4.php";
 ?>
@@ -9,8 +9,8 @@ require "../common_ap_V1.4.php";
 
 	<ul>
 		<li><a href="201_alta_osc_<?php echo escape($vers);?>.php"><strong>Alta OSC</strong></a> - Alta OSC</li>
-		<li><a href="202_admin_osc_<?php echo escape($vers);?>.php"><strong>Actualizar un VOL</strong></a> - Buscar y modif Voluntario</li>
-		
+		<li><a href="202_buscar_osc_<?php echo escape($vers);?>.php"><strong>Buscar OSC p/Actualizar</strong></a> - Buscar OSC y Actualizar</li><br>
+		<a href="../index_ap_<?php echo escape($vers);?>.php">Back to home<class=button /a>
 <!--
 		<h3>---------------------------------------------------------------------------------------------</h3>
 		<li><a href="buscar_vol_x_apell_ap_<?php echo escape($vers);?>.php"><strong>Asignar VOL x Apellido</strong></a> - Buscar VOL x Apellido p/ asignar a proyecto</li>
@@ -22,4 +22,4 @@ require "../common_ap_V1.4.php";
 
 	</ul>
 
-<?php include "../templates/footer_osc_V1.4.php"; ?>
+<?php include "../templates/footer_osc.php"; ?>
--- OSCs/201_alta_osc_V1.4.php
+++ OSCs/201_alta_osc_V1.4.php
@@ -12,97 +12,70 @@ if (isset($_POST['submit'])) {
     try  {
         $connection = new PDO($dsn, $username, $password, $options);
         
-        $new_user = array(
-            "nombres" => $_POST['nombres'],
-            "apellido"  => $_POST['apellido'],
-            "dni"     => $_POST['dni'],
-            "profesion"       => $_POST['profesion'],
-            "email_1"  => $_POST['email_1'],
-            "email_2"  => $_POST['email_2']
+        $new_osc = array(
+            "osc_nombre" => $_POST['osc_nombre'],
+            "osc_notas"  => $_POST['osc_notas'],
+
         );
         $sql = sprintf(
                 "INSERT INTO %s (%s) values (%s)",
-                "t_users1",
-                implode(", ", array_keys($new_user)),
-                ":" . implode(", :", array_keys($new_user))
+                "t_osc",
+                implode(", ", array_keys($new_osc)),
+                ":" . implode(", :", array_keys($new_osc))
         );
         $error = "";
         $statement = $connection->prepare($sql);
-        $statement->execute($new_user);
+        $statement->execute($new_osc);
         
     } catch(PDOException $error) {
        // echo $sql . "<br>" . $error->getMessage();
-        echo "Hubo un error en el insert de: " , $_POST['apellido'] . "<br>" . $error->getMessage();
+        echo "Hubo un error en el insert de: " , $_POST['osc_nombre'] . "<br>" . $error->getMessage();
     }
 }
 ?>
 
-<?php require "templates/header.php"; ?>
+<?php require "../templates/header_osc.php"; ?>
 
 
 <?php if (isset($_POST['submit']) && $statement && !$error){ ?>
-    <blockquote><?php echo $_POST['apellido'] , ", " , $_POST['nombres']; ?> agregada/o a la base de Aportes.</blockquote>
-
-<?php } ?>
-
-<?php
-
-try {
-  require "./config_ap_V1.4.php";
-
-  $conn_prof = new PDO($dsn, $username, $password, $options);
-  
-  $sql_prof = "SELECT profesion  FROM t_profesiones ORDER BY profesion";
-
-  $stat_prof = $conn_prof->prepare($sql_prof);
-  
-  $stat_prof->execute();
-
-  $a_prof = $stat_prof->fetchAll();
-} catch(PDOException $error) {
-  echo $sql_prof . "<br>" . $error->getMessage();
-}         
-
-?>
+    <blockquote><?php echo $_POST['osc_nombre'] ?> Registrada en la base de Aportes.</blockquote><br>
+    
+    <td><a href="202_admin_osc_<?php echo escape($vers);?>?dni=<?php echo $_POST['osc_nombre']; ?>
+				">Administrar OSC</a></td><br>
+    <a href="../index_ap_V1.4.php">Back to home</a>
+    
+<?php require "../templates/footer_osc.php"; ?>
+<?php 
+exit;
+} ?>
 
 
-
-<h2>Agregar Voluntario</h2>
+<h2>Agregar OSC</h2>
 
 <form method="post">
-	<label for="nombres">Nombres</label>
-		<input type="text" name="nombres" id="nombres">
-	<label for="apellido">Apellidos</label>
-		<input type="text" name="apellido" id="apellido">
-	<label for="dni">DNI</label>
-	<input type="text" name="dni" id="dni">
-			<p>
-			<label for="profesion">Profesion</label> 
-			<select name="profesion">
-			<option value="N/D">Seleccione...</option> 
-			<?php foreach ($a_prof as $profe) { ?>
-				<option value="<?php echo $profe["profesion"]; ?>"><?php echo $profe["profesion"]; ?></option>
-			<?php } ?>
-			</select>
-			</p>
-	<label for="email_1">Email Principal</label>
-		<input type="text" name="email_1" id="email_1">
-	<label for="email_2">Email Alternativo</label>
-		<input type="text" name="email_2" id="email_2">	
-	<input type="submit" name="submit" value="Guardar">
+	<label for="osc_nombre">Nombre OSC</label><br>
+		<input type="text" name="osc_nombre" id="osc_nombre"><br>
+	<label for="osc_notas">Notas sobre OSC</label><br>
+		<input type="text" name="osc_notas" id="osc_notas"><br><br>
+
+	<input type="submit" name="submit" value="Agregar OSC">
 </form>
 
 <?php if (isset($_POST['submit']) && $statement && !$error){ ?>
    
-				<td><a href="create_restr_ap_V1.4.php?dni=<?php echo $_POST['dni']; ?>
-				&apellido=<?php echo $_POST['apellido']; ?>
-				&nombres=<?php echo $_POST['nombres']; ?>
-				">Agreg Datos RESTR VOL</a></td>
+				<td><a href="202_admin_osc_<?php echo escape($vers);?>?dni=<?php echo $_POST['osc']; ?>
+				">Administrar OSC</a></td>
 <?php } ?>
 
+<?php
+//require "../config_ap_V1.4.php";
+//require "../common_ap_V1.4.php";
+?>
 
-<a href="index_ap_V1.4.php">Back to home</a>
-
-<?php require "templates/footer.php"; ?>
+<!--
+<a href="../index_ap_<?php echo escape($vers);?>.php">Back to home</a>
+-->
+<a href="../index_ap_V1.4.php">Back to home</a>
+<?php require "../templates/footer_osc.php"; ?>
 
 
