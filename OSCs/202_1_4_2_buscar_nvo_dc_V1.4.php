9990a1e0b9910de91a8c4106a5e55a7166f02c22
Basilio Dubko <badubko@gmail.com>
2019-02-15 13:13:52 -0300

    Cambiado criterio busqueda DC
    Ahora busca con estado != De_baja
    AND != ND_Temp

--- OSCs/202_1_4_2_buscar_nvo_dc_V1.4.php
+++ OSCs/202_1_4_2_buscar_nvo_dc_V1.4.php
@@ -18,7 +18,7 @@ if (isset($_POST['submit'])) {
 						dni, apellido, nombres, rol, tel_1, email_1,estado
 				FROM    us1_us2
 				WHERE
-				apellido LIKE :apellido AND (estado='Disponible') AND (rol='VC' OR rol='DC')
+				apellido LIKE :apellido AND (estado !='Be_baja' AND estado != 'ND_Temp' ) AND (rol='VC' OR rol='DC')
 				AND ( dni != :dc_tit_ant) AND ( dni != :dc_supl_ant) 
 				ORDER BY apellido;" ;
 
@@ -44,7 +44,7 @@ if (isset($_POST['submit'])) {
 	if ($result && $statement->rowCount() > 0) { ?>
 		<h3>Buscar DC por apellido</h3>
 		<h3>para ASIGNAR a OSC: <?php echo escape($_GET['osc_nombre']); ?> como: DC <?php echo escape($_GET['rol_dc']); ?> </h3>
-		<h3>Resultados para (rol = DC o VC y estado = Disponible )</h3>
+		<h3>Resultados para rol = DC o VC y estado != De_Baja y != ND_Temp </h3>
 <!--
 		<a href="../index_ap_<?php echo escape($vers);?>.php">Back to home</a>
 -->
