25590871bdcb562bdbecbca49be10d97b3c0b19e
Basilio Dubko <badubko@gmail.com>
2019-02-19 22:44:54 -0300

    Version inicial asigna dc

--- OSCs/202_1_4_2_1_asigna_dc_V1.4.php
+++ OSCs/202_1_4_2_1_asigna_dc_V1.4.php
@@ -1,9 +1,65 @@
 <?php 
 
+require "../config_ap_V1.4.php";
+require "../common_ap_V1.4.php";
 
-		require "../config_ap_V1.4.php";
-		require "../common_ap_V1.4.php";
+if (isset($_POST['submit'])) {
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
 
+		if ( $osc_rol_dc = 'Titular') {
+			$osc =[
+			  "osc_nombre"      		=> $_GET['osc_nombre'],   
+			  "osc_dc_tit"  			=> $_GET['dni_nvo'],
+			  "osc_f_titular" 				=> $_POST['osc_f_titular'],
+					];
+			$sql = "UPDATE t_osc
+					SET osc_nombre = :osc_nombre,
+						osc_dc_tit = :osc_dc_tit,
+						osc_f_titular = :osc_f_titular 
+					 WHERE osc_nombre = :osc_nombre";
+		} else {
+			  $osc =[
+			  "osc_nombre"      		=> $_GET['osc_nombre'],   
+			  "osc_dc_supl"  			=> $_GET['dni_nvo'],
+			  "osc_f_supl" 				=> $_POST['osc_f_supl'],
+					];
+			$sql = "UPDATE t_osc
+					SET osc_nombre = :osc_nombre,
+						osc_dc_supl = :osc_dc_supl,
+						osc_f_supl = :osc_f_supl 
+					 WHERE osc_nombre = :osc_nombre";        
+		}    
+
+   //$apellido = $_POST['apellido'];
+   //$nombres = $_POST['nombres'];
+    
+
+ 
+  $statement = $connection->prepare($sql);
+  $statement->execute($osc);
+  
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+}
+?>
+
+<?php if (isset($_POST['submit']) && $statement && !$error ) : ?>
+	<blockquote>
+	<?php echo escape($_GET['rol_dc']) , ", para " , escape($_GET['osc_nombre']); ?> Fue actualizada/o OK. 
+	</blockquote>
+	    <br>
+		<a href="202_buscar_osc_<?php echo escape($vers);?>.php">Buscar otra OSC p/ Agregar o Actualizar datos</a>
+		<br>
+		<br>
+		<a href="../index_ap_<?php echo escape($vers);?>.php">Back to home</a>
+		<?php require "../templates/footer_osc.php"; ?>
+<?php 
+exit;
+endif; ?>
+
+<?php
 $osc_nombre = $_GET['osc_nombre'];
 
 $dni_ant=$_GET['dni_ant'];
@@ -31,7 +87,7 @@ table, th, td {
 </style>
 
 <?php
-// Si no es desasignacion
+// Si es asignacion
 
 if ( $dni_nvo != 1 AND $dni_nvo !=2 ) { ?>
 	
@@ -73,7 +129,7 @@ else
 	</tbody>
 </table><br><br>
 
-
+<form method="post">
 	<?php 	switch ($_GET['rol_dc']) {
 			case 'Titular':?>
 				<label for="osc_f_titular">Fecha Cambio Tit</label>
@@ -91,8 +147,15 @@ else
 	?> 			 
 				 		
 	<label for="osc_comentarios_dc">Comentario sobre este cambio<br></label>
-		<input type="text" name="osc_comentarios_dc" id="osc_comentarios_dc">
+	<input type="text" name="osc_comentarios_dc" id="osc_comentarios_dc">
 	<input type="submit" name="submit" value="Confirmar">
+</form>
+        <br>
+		<a href="202_buscar_osc_<?php echo escape($vers);?>.php">Buscar otra OSC p/ Agregar o Actualizar datos</a>
+		<br>
+		<br>
+		<a href="../index_ap_<?php echo escape($vers);?>.php">Back to home</a>
+		<?php require "../templates/footer_osc.php"; ?>
 
 <?php
 exit;
