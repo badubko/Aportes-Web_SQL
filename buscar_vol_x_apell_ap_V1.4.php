1c45e2bafe6f7eebe7b23e189c86d026337e6991
Basilio Dubko <badubko@gmail.com>
2019-02-28 18:23:42 -0300

    Corregido sql de busqueda y listado
    Ahora toma los datos de la view us1_us2

--- buscar_vol_x_apell_ap_V1.4.php
+++ buscar_vol_x_apell_ap_V1.4.php
@@ -15,26 +15,22 @@ if (isset($_POST['submit'])) {
 		$connection = new PDO($dsn, $username, $password, $options);
 
 		// Se elimino "AND 	(estado != 'ND_Temp' )"
+		// el estado= 'Interno' no existira mas en el futuro
 
 		$sql = "SELECT
-						t_users1.dni, t_users1.apellido, t_users1.nombres
-				FROM    t_users1
-				WHERE
-					dni IN (			
-							SELECT dni 
-							FROM t_users2 
-						     WHERE 	( (		(estado != 'De_Baja') AND 	(estado != 'Interno' ) ) 
-								AND ( 		(rol = 'Vol' ) 
-										OR  (rol = 'VC') 
-										OR	(rol = 'DC')	) 
-								AND dni IN
-								(SELECT dni
-									FROM t_users1 
-									WHERE apellido LIKE :apellido
-								)						     
-									)		
+						dni, apellido, nombres, rol, estado, tel_1, email_1
+				FROM    us1_us2
+				WHERE 	( (		(estado != 'De_Baja') AND 	(estado != 'Interno' ) ) 
+								
+						AND ( 		rol = 'Vol'  
+								OR  rol = 'VC' 
+								OR	rol = 'DC'	)
+								 
+							AND apellido LIKE :apellido
+														     
+									
 				 
-							) ORDER BY apellido;" ;
+							) ORDER BY rol DESC, estado, apellido;" ;
 
 		$apellido = $_POST['apellido'];
 
@@ -55,15 +51,18 @@ if (isset($_POST['submit'])) {
 	if ($result && $statement->rowCount() > 0) { ?>
 		<h3>Buscar Voluntario por apellido</h3>
 		<h3>para ASIGNAR a un Proyecto</h3>
-		<h3>Resultados para (estado != ND_Temp y estado != De_Baja)</h3>
+		<h3>Resultados para (estado != De_Baja)</h3>
 		<a href="index_ap_<?php echo escape($vers);?>.php">Back to home</a>
 		<table>
 			<thead>
 				<tr>
-					<th>dni</th>
-					<th>apellido</th>
-					<th>nombre</th>
+					<th>DNI</th>
+					<th>Apellido</th>
+					<th>Nombres</th>
+					<th>Rol</th>
 					<th>Estado Act</th>
+					<th>Tel_1</th>
+					<th>email_1</th>
 					<th>Seleccionar</th>
 <!--					
 					<th>Editar</th>
@@ -76,52 +75,17 @@ if (isset($_POST['submit'])) {
 				<td><?php echo escape($row["dni"]); ?></td>
 				<td><?php echo escape($row["apellido"]); ?></td>
 				<td><?php echo escape($row["nombres"]); ?></td>
-				
-				<?php
-					try {	
-						require "./config_ap_V1.4.php";
-						$dni_estado = $row["dni"] ;
-						$connection = new PDO($dsn, $username, $password, $options);
-						$sql_estado = "SELECT 	estado FROM t_users2 WHERE dni = :dni_estado" ;
-						$stat_estado = $connection->prepare($sql_estado);
-						$stat_estado->bindParam(':dni_estado', $dni_estado, PDO::PARAM_STR);
-						$stat_estado->execute();
-						$result_estado = $stat_estado->fetchAll();
-						} catch(PDOException $err_estado) {
-						// echo $sql_estado . "<br>" . $err_estado->getMessage();
-				?>
-						<td>No Disponible</td>
-						
-														<?php	}
-				?>
-<!--
-				<td><?php echo escape($dni_estado); ?></td>
--->
-				<?php foreach ($result_estado as $row_est ) { ?>
-					<td><?php echo $row_est["estado"] ; ?></td>
-				<?php											} ?>
-<!--					
-				<td><?php echo escape($row["profesion"]); ?></td>
+				<td><?php echo escape($row["rol"]); ?></td>
+				<td><?php echo escape($row["estado"]); ?></td>
+				<td><?php echo escape($row["tel_1"]); ?></td>
 				<td><?php echo escape($row["email_1"]); ?></td>
-				<td><?php echo escape($row["email_2"]); ?></td>
-				<td><?php echo escape($row["last_update"]); ?></td>
-				<td><a href="modif_est_vol_ap_<?php echo escape($vers);?>.php?dni=<?php echo escape($row["dni"]); ?>">Estado Vol</a></td>
--->
+
 				<td><a href="sel_osc_p_proy_ap_<?php echo escape($vers);?>.php?dni=<?php echo escape($row["dni"]); ?>
 				&apellido=<?php echo escape($row["apellido"]); ?>
 				&nombres=<?php echo escape($row["nombres"]); ?>
-				&est_act=<?php echo escape($row_est["estado"]); ?>
+				&est_act=<?php echo escape($row["estado"]); ?>
 				">P/Asignar</a></td>
-<!--				
-				<td><a href="update-single_restr_ap_<?php echo escape($vers);?>.php?dni=<?php echo escape($row["dni"]); ?>
-				&apellido=<?php echo escape($row["apellido"]); ?>
-				&nombres=<?php echo escape($row["nombres"]); ?>
-				">RESTR VOL</a></td>
-				<td><a href="listar-esp_ap_<?php echo escape($vers);?>.php?dni=<?php echo escape($row["dni"]); ?>
-				&apellido=<?php echo escape($row["apellido"]); ?>
-				&nombres=<?php echo escape($row["nombres"]); ?>
-				">Espec</a></td>
--->				
+
 			</tr>
 		<?php } ?> 
 			</tbody>
