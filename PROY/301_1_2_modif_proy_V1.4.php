ad7483756560f47378360c9609a1123ba2a447d3
Basilio Dubko <badubko@gmail.com>
2019-03-04 17:21:06 -0300

    Version semi-completa

--- PROY/301_1_2_modif_proy_V1.4.php
+++ PROY/301_1_2_modif_proy_V1.4.php
@@ -8,46 +8,30 @@ require "../config_ap_V1.4.php";
 require "../common_ap_V1.4.php";
 
 
-if (isset($_GET['osc_nombre'])) {
+if (isset($_GET['p_num_corr_proy'])) {
   try {
 
     $connection = new PDO($dsn, $username, $password, $options);
-    $osc_nombre = $_GET['osc_nombre'];
+    $osc_nombre = 		$_GET['osc_nombre'];
+    $p_num_corr_proy = 	$_GET['p_num_corr_proy'];
+    $p_nombre_proy = 	$_GET['p_nombre_proy'];
     
-    $sql1 = "SELECT osc_nombre,
-    				osc_calle_1,
-    				osc_num_1,
-    				osc_ciudad_1,
-    				osc_cp_1,
-    				osc_prov_1,
-    				osc_calle_2,
-    				osc_num_2,
-    				osc_ciudad_2,
-    				osc_cp_2,
-    				osc_prov_2,
-    				osc_pag_web,
-    				osc_estado,
-    				osc_acuerdo,
-    				osc_notas,
-    				osc_dc_tit,
-    				osc_f_titular,
-    				osc_dc_supl,
-    				osc_f_supl,
-    				last_update
-    FROM t_osc WHERE osc_nombre = :osc_nombre";
+    
+    $sql1 = "SELECT * FROM t_proyectos WHERE p_num_corr_proy = :p_num_corr_proy";
+    
     
     $statement = $connection->prepare($sql1);
-    $statement->bindValue(':osc_nombre', $osc_nombre);
+    $statement->bindValue(':p_num_corr_proy', $p_num_corr_proy);
     $statement->execute();
     
-    $osc = $statement->fetch(PDO::FETCH_ASSOC);
+    $proy = $statement->fetch(PDO::FETCH_ASSOC);
     $count = $statement->rowCount();
      
   } catch(PDOException $error) {
       echo $sql1 . "<br>" . $error->getMessage();
   }
 } else {
-    echo "Algo salio mal AQUI!" ;
+    echo "Algo salio mal AQUI! No se encontro el proyecto" ;
     exit;
 }
 ?>
@@ -58,61 +42,62 @@ if (isset($_GET['osc_nombre'])) {
 
 if (isset($_POST['submit'])) {
   try {
-//    $connection = new PDO($dsn, $username, $password, $options);
-    $upd_osc =[
-			"osc_nombre" 	=> $_POST['osc_nombre'],
-			"osc_calle_1" 	=> $_POST['osc_calle_1'],
-			"osc_num_1" 	=> $_POST['osc_num_1'],
-			"osc_ciudad_1" 	=> $_POST['osc_ciudad_1'],
-			"osc_cp_1" 		=> $_POST['osc_cp_1'],
-			"osc_prov_1" 	=> $_POST['osc_prov_1'],
+
+    $upd_proy =[
+		
+            "p_num_corr_proy"			=> $_POST['p_num_corr_proy'],
+    
+
+            "p_fecha_pre_proy"			=> $_POST['p_fecha_pre_proy'],
+            "p_fecha_present_vol"		=> $_POST['p_fecha_present_vol'],
+            
+            "p_fecha_mitad_proy_estim"	=> $_POST['p_fecha_mitad_proy_estim'],
+            "p_fecha_mitad_proy_real"	=> $_POST['p_fecha_mitad_proy_real'],
+
+            "p_fecha_cierre_proy_estim" => $_POST['p_fecha_cierre_proy_estim'],
+            "p_fecha_cierre_proy_real"	=> $_POST['p_fecha_cierre_proy_real'],
+            
+            
+
+            "p_dup_si_no"				=> $_POST['p_dup_si_no'],
+            "p_fecha_dup" 				=> $_POST['p_fecha_dup'],
+            "p_link_a_dup"				=> $_POST['p_link_a_dup'],
 			
-			"osc_calle_2" 	=> $_POST['osc_calle_2'],
-			"osc_num_2" 	=> $_POST['osc_num_2'],
-			"osc_ciudad_2" 	=> $_POST['osc_ciudad_2'],
-			"osc_cp_2" 		=> $_POST['osc_cp_2'],
-			"osc_prov_2" 	=> $_POST['osc_prov_2'],
-			
-			"osc_pag_web" 	=> $_POST['osc_pag_web'],
-			"osc_acuerdo" 	=> $_POST['osc_acuerdo'],
-			"osc_notas" 	=> $_POST['osc_notas']
 			];
 
     
-   //$apellido = $_POST['apellido'];
-   //$nombres = $_POST['nombres'];
-    
-    $sql2 = "UPDATE t_osc
+
+    $sql2 = "UPDATE t_proyectos
             SET  
-					osc_calle_1 = :osc_calle_1,
-    				osc_num_1 = :osc_num_1,
-    				osc_ciudad_1 = :osc_ciudad_1,
-    				osc_cp_1 = :osc_cp_1,
-    				osc_prov_1 = :osc_prov_1,
+					p_num_corr_proy = :p_num_corr_proy,
+					p_fecha_pre_proy = :p_fecha_pre_proy,
+					p_fecha_present_vol = :p_fecha_present_vol,
+					
+					p_fecha_mitad_proy_estim = :p_fecha_mitad_proy_estim,
+					p_fecha_mitad_proy_real = :p_fecha_mitad_proy_real,
+					
+					p_fecha_cierre_proy_estim = :p_fecha_cierre_proy_estim,
+					p_fecha_cierre_proy_real = :p_fecha_cierre_proy_real,
+					
+					p_dup_si_no = :p_dup_si_no,
+					p_fecha_dup = :p_fecha_dup,
+					p_link_a_dup = :p_link_a_dup,
     				
-    				osc_calle_2 = :osc_calle_2,
-    				osc_num_2 = :osc_num_2,
-    				osc_ciudad_2 = :osc_ciudad_2,
-    				osc_cp_2 = :osc_cp_2,
-    				osc_prov_2 = :osc_prov_2,
-    				
-    				osc_pag_web = :osc_pag_web,
-    				osc_acuerdo = :osc_acuerdo,
-    				osc_notas = :osc_notas
-             WHERE osc_nombre = :osc_nombre";
+             WHERE p_num_corr_proy = :p_num_corr_proy";
              
   $error="";
   $statement = $connection->prepare($sql2);
-  $statement->execute($upd_osc);
+  $statement->execute($upd_proy);
   
   
   } catch(PDOException $error) {
       echo $sql2 . "<br>" . $error->getMessage();
+      exit;
   }
   
 if ( $statement && !$error) : ?>
 	<blockquote>
-	OSC: <?php echo escape($_GET['osc_nombre'])  ?> Fue actualizada OK. <br>
+	Proyecto: <?php echo escape($_GET['p_num_corr_proy'])  ?> Fue actualizado OK. <br>
 	</blockquote>
 	<a href="202_buscar_osc_<?php echo escape($vers);?>.php">Buscar otra OSC p/Actualizar</a> - Buscar otra OSC p/ Agregar o Actualizar datos	
 	<br><a href="../index_ap_<?php echo escape($vers);?>.php">Back to home</a>
@@ -122,47 +107,43 @@ exit;
 
 ?>
 
-<h3>Modificar datos de OSC <?php echo escape($_GET['osc_nombre'])?> </h3>
+<h3>Modificar datos de un Proyecto de <?php echo escape($_GET['p_num_corr_proy'])?> </h3>
 
 <?php if ( $count != 0 ) { ?>
 <form method="post">
-	
-    <?php foreach ($osc as $key => $value) : ?>
+<table>
+    <?php foreach ($proy as $key => $value) : ?>
     <?php switch ($key) {
-			case 'osc_acuerdo': 
+			case 'p_num_corr_proy':
+			case 'osc_nombre': 
+			case 'p_nombre_proy':  
+			case 'p_ultimo_estado': 
+			case 'last_update':  
 	?>	
-			<label for="osc_acuerdo">Acuerdo  </label> 
-			<select name="osc_acuerdo">
-			<option value="<?php echo escape($value); ?>"><?php echo escape($value); ?></option>
-				<option value="Firmado">Firmado</option>			
-				<option value="Pendiente">Pendiente</option>
-				<option value="N/D">N/D</option>
-			</select>	
-		    <br>
-	<?php	break;
-			case 'osc_estado': 
+			<tr>
+<!--
+				 <td><strong><?php echo escape (ucfirst(str_replace("p_","",$key))); ?></strong></td>
+				 <td><?php echo escape($value); ?></td> 
+-->
+				<label for="<?php echo $key; ?>"><?php echo (ucfirst(str_replace("p_","",$key))); ?><br></label>    
+				<input type="text" name="<?php echo $key; ?>" iden="<?php echo $key; ?>" value="<?php echo escape($value); ?>" <?php echo ( 'readonly' ); ?>><br>
+			</tr>	
+		   
+	<?php 	break;
+	
+			default: 
 	?>
-		<label for="<?php echo $key; ?>"><?php echo ucfirst($key); ?><br></label>    
-	    <input type="text" name="<?php echo $key; ?>" iden="<?php echo $key; ?>" value="<?php echo escape($value); ?>" <?php echo ( 'readonly' ); ?>><br>
+			<tr>
+			<label for="<?php echo $key; ?>"><?php echo (ucfirst(str_replace("p_","",$key))); ?><br></label>    
+			<input type="text" name="<?php echo $key; ?>" iden="<?php echo $key; ?>" value="<?php echo escape($value); ?>" ><br>
+			</tr>
 	<?php	break;
-	 	    // Los DCs se administran en otro menu...
-	 	    // La opcion de hacerlo se presenta mas abajo.
-			case 'osc_dc_tit':
-			case 'osc_f_titular': 
-			case 'osc_dc_supl': 
-			case 'osc_f_supl': 
-			break;
-		default:		
-	?>
-		<label for="<?php echo $key; ?>"><?php echo ucfirst($key); ?><br></label>    
-	    <input type="text" name="<?php echo $key; ?>" iden="<?php echo $key; ?>" value="<?php echo escape($value); ?>" ><br>
-	<?php	break;
-						}	?>
+					}	?>
     <?php endforeach; ?> 
     <input type="submit" name="submit" value="Guardar">
-
+</table>
 </form>
-
+	
 	<li><a href="202_1_4_asign_desasign_dcs_<?php echo escape($vers);?>.php
 				?osc_nombre=<?php echo escape($_GET["osc_nombre"]); ?>
 				">Ver o Asignar/Desasignar DCs</a> - Asignar o Desasignar DCs para OSC</li>
