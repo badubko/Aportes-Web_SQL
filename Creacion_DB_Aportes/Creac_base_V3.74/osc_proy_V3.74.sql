39af1d50baaa253ca3a640a3c9ece18c38246b70
Basilio Dubko <badubko@gmail.com>
2019-03-18 19:27:43 -0300

    Agregado de comentario que este view ya esta incluido
    en la creacion de la base

--- Creacion_DB_Aportes/Creac_base_V3.74/osc_proy_V3.74.sql
+++ Creacion_DB_Aportes/Creac_base_V3.74/osc_proy_V3.74.sql
@@ -1,3 +1,5 @@
+-- Incluida en crea_db_aportes_V3.74.sql
+
 CREATE VIEW osc_proy AS
 SELECT DISTINCT	t_hist_user_proy.dni, t_proyectos.p_num_corr_proy , t_proyectos.osc_nombre, 
 t_proyectos.p_nombre_proy , t_proyectos.p_ultimo_estado
