e3a71a3252e5cb42d4a449ed7a4713f5e80ee3d0
Basilio Dubko <badubko@gmail.com>
2019-03-18 20:31:22 -0300

    Modificacion vs
    Titulos corregidos
    Ahora  presenta 2 opciones de acuerdo a estado OSC
    Si OSC Identificada o Contactada -> Cambiar estado
    Si En_Conversacion o En_Actividad -> Crear proy o modif

--- PROY/301_buscar_osc_p_proy_V1.4.php
+++ PROY/301_buscar_osc_p_proy_V1.4.php
@@ -33,13 +33,13 @@ if (isset($_POST['submit'])) {
 													}
 }
 ?>
-<?php require "../templates/header_osc.php"; ?>
+<?php require "../templates/header_proy.php"; ?>
 		
 <?php  
 if (isset($_POST['submit'])) {
 	if ($result && $statement->rowCount() > 0) { ?>
 
-		<h3>Buscar OSC por nombre aproximado</h3>
+		<h3>Elegir OSC</h3>
 
 		<a href="../index_ap_<?php echo escape($vers);?>.php">Back to home</a><br><br>
 		
@@ -60,10 +60,25 @@ if (isset($_POST['submit'])) {
 	<?php foreach ($result as $row) { ?>
 			<tr>
 				<td><?php echo escape($row["osc_nombre"]); ?></td>
-				<td><?php echo escape($row["osc_estado"]); ?></td>		
-				<td><a href="301_1_buscar_proy_osc_<?php echo escape($vers);?>.php
+				<td><?php echo escape($row["osc_estado"]); ?></td>	
+				
+				<?php switch ($row["osc_estado"]) {
+						case 'Identificada':
+						case 'Contactada':?>
+				<td><a href="../OSCs/204_cambiar_est_osc<?php echo escape($vers);?>.php
 				?osc_nombre=<?php echo escape($row["osc_nombre"]); ?>
-				">Ver proy de OSC o crear uno nuevo</a></td>
+				">Cambiar estado OSC </a></td>
+				
+				<?php	break;	
+						case 'En_Conversacion': 	
+						case 'En_Actividad': 		?>
+			
+						<td><a href="301_1_buscar_proy_osc_<?php echo escape($vers);?>.php
+						?osc_nombre=<?php echo escape($row["osc_nombre"]); ?>
+						">Ver proy de OSC o crear uno nuevo</a></td>
+				<?php	break;
+						}       ?> 
+				
 			</tr>
 		<?php } ?> 
 			</tbody>
@@ -87,7 +102,7 @@ exit;
 		
 
 <h3>Buscar OSC por nombre aproximado</h3>
-<h3>para para agregar o actualizar datos</h3>
+<h3>para para administrar proyectos</h3>
 
 <form method="post">
 	<label for="osc_nombre">OSC (A%  %A%  %)</label>
