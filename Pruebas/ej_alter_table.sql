0c359835f3c13c1b19e2e66be4d7321f1f3f6788
Basilio Dubko <badubko@gmail.com>
2019-03-19 19:36:00 -0300

    Agregado de id_truch en t_osc_logs_dc
    Solo para parche

--- Pruebas/ej_alter_table.sql
+++ Pruebas/ej_alter_table.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `t_hist_user_proy` ADD COLUMN `coment_desasign` VARCHAR (256) DEFAULT "No Comments"  AFTER  coment_asignac;
 
 alter table t_osc_contactos add  `id_truch` INT UNSIGNED NOT NULL AUTO_INCREMENT AFTER last_update , ADD PRIMARY KEY (`id_truch`);
+alter table t_osc_logs_estado add  `id_truch` INT UNSIGNED NOT NULL AUTO_INCREMENT AFTER last_update , ADD PRIMARY KEY (`id_truch`);
