5c4706037192ef3b09632d9c88a06c740931e12a
Basilio Dubko <badubko@gmail.com>
2019-03-19 17:51:48 -0300

    Se agrego el pasaje de la variable osc_estado

--- OSCs/202_1_admin_osc_V1.4.php
+++ OSCs/202_1_admin_osc_V1.4.php
@@ -28,7 +28,12 @@ require "../common_ap_V1.4.php";
 				
 		<li><a href="202_1_4_asign_desasign_dcs_<?php echo escape($vers);?>.php
 				?osc_nombre=<?php echo escape($_GET["osc_nombre"]); ?>
-				"><strong>Ver o Asignar/Desasignar DCs</strong></a> - Asignar o Desasignar DCs para OSC</li><br><br>
+				"><strong>Ver o Asignar/Desasignar DCs</strong></a> - Ver y/o Asignar/Desasignar DCs para OSC</li>
+				
+		<li><a href="204_cambiar_est_osc_<?php echo escape($vers);?>.php
+				?osc_nombre=<?php echo escape($_GET["osc_nombre"]); ?>
+				&osc_estado=<?php echo escape($_GET["osc_estado"]); ?>
+				"><strong>Cambiar estado OSC</strong></a> - Cambiar el estado de la OSC (Limitado)</li><br>
 		
 		<li><a href="202_buscar_osc_<?php echo escape($vers);?>.php"><strong>Buscar otra OSC p/Actualizar</strong></a> - Buscar otra OSC p/ Agregar o Actualizar datos</li><br>
 						
