64b444fb8758fe99085fbb7481f7fd3f90f3a5c6
Basilio Dubko <badubko@gmail.com>
2019-03-19 15:21:23 -0300

    Evolucion del cambio estado osc

--- OSCs/204_cambiar_est_osc_V1.4.php
+++ OSCs/204_cambiar_est_osc_V1.4.php
@@ -15,6 +15,54 @@
  *
  */
 
+if (isset($_POST['submit_est'])) {
+	try {	
+		require "../config_ap_V1.4.php";
+		
+
+		$connection = new PDO($dsn, $username, $password, $options);
+   
+        $osc_nombre = $_GET['osc_nombre'];
+		$osc_estado = $_GET['osc_estado'];
+		$osc_coment_estado = $_POST['osc_coment_estado'];
+		// dni del DP viene del config por ahora hardcoded.
+		$dni = $dni_dp;
+		
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		// prepare sql and bind parameters
+		$stmt = $connection->prepare(" INSERT INTO t_logs_estado_user (dni, estado, consideraciones) VALUES (:dni, :estado, :consideraciones)");
+    $stmt->bindParam(':dni', $dni);
+    $stmt->bindParam(':estado', $estado);
+    $stmt->bindParam(':consideraciones', $consideraciones);
+    
+    // insertar nuevo estado
+
+    $stmt->execute();
+    // $result = $stmt->fetchAll();
+    $e = "";
+
+			
+			$sql = "SELECT dni, apellido, nombres, rol, estado FROM us1_us2 WHERE 
+					  (apellido LIKE :apellido) AND
+					 ((estado != 'De_Baja') AND (estado != 'Asignado' )) AND 
+					 ((rol = 'Vol' ) OR (rol = 'VC') OR (rol = 'DC')) 
+						ORDER BY apellido, nombres;" ;				
+							
+							
+		$apellido = $_POST['apellido'];
+
+		$statement = $connection->prepare($sql);
+		$statement->bindParam(':apellido', $apellido, PDO::PARAM_STR);
+		$statement->execute();
+
+		$result = $statement->fetchAll();
+	} catch(PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+													}
+}
+
+
 // Buscamos los estados posibles a partir del estado actual
 try {	
 		require "../config_ap_V1.4.php";
@@ -44,20 +92,25 @@ try {
 ?>
 
 <h3>Elegir un Nuevo estado para la OSC</h3>
+<h4>(Algunos estados son automaticos y no se pueden cambiar aqui)</h4>
 
-		<a href="../index_ap_<?php echo escape($vers);?>.php">Back to home</a><br><br>
+
+		
 		
 		<style>
 		table, th, td {
 		border: 1px solid black; }
 		</style>
 		
+<form method="post">		
 		<table>
 			<thead>
 				<tr>
 					<th>OSC</th>
 					<th>Estado Actual</th>
 					<th>Nuevo Estado</th>
+					<th>Comentario sobre cambio de estado</th>
+					<th>Acción</th>
 				</tr>
 			</thead>
 			
@@ -67,7 +120,9 @@ try {
 				<td><?php echo escape($_GET['osc_estado']); ?></td>	
 				<td>
 				<p>
+<!--
 					<label for="estado">Nuevo Estado</label> 
+-->
 					<select name="estado">
 					<option value="">Seleccione...</option>
 					<?php foreach ($result as $est_pos) { ?>
@@ -76,9 +131,15 @@ try {
 					</select>
 				</p>
 				</td>
- 		
+<!--
+				<label for="consideraciones">Consideraciones</label>
+-->
+				<td><input type="text" name="osc_coment_estado" id="osc_coment_estado"></td>
+				<td><input type="submit" name="submit_est" value="Guardar"></td>
 			</tr>
 			</tbody>
 		</table>
 		
+</form>		
+<a href="../index_ap_<?php echo escape($vers);?>.php">Back to home</a><br><br>		
 <?php exit; ?>		
