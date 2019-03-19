2013010ba77e9b17fb92f77850bf846dfb73db88
Basilio Dubko <badubko@gmail.com>
2019-03-19 15:21:03 -0300

    Agregado del dni del DP por ahora

--- config_ap_V1.4.php
+++ config_ap_V1.4.php
@@ -22,6 +22,11 @@ $nom_supl_fict='No Asignado';
 $f_min='2000-01-01';
 $f_max='2100-01-01';
 
+// El dni del DP por ahora hardcoded. En el futuro...
+// Solo DIOS sabe como sera...
+
+$dni_dp='1000999'
+
 $vers        = "V1.4";
 $host       = "localhost";
 $username   = "root";
