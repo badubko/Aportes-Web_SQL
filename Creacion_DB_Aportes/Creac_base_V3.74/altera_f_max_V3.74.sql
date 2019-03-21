f8539926cab3c9f86926bae8b602e17de850b565
Basilio Dubko <badubko@gmail.com>
2019-03-21 15:41:45 -0300

    Script para cambiar fecha default sin recrear tabla

---
+++ Creacion_DB_Aportes/Creac_base_V3.74/altera_f_max_V3.74.sql
@@ -0,0 +1,60 @@
+-- Este script cambia la fecha de default en la tabla t_proyectos
+-- sin tener que regenerar la tabla o la base
+-- El valor de la fecha deberia coincidir con la f_max en config
+
+USE aportes_V3_74;
+SET @FE = '2100-01-01';
+SELECT @FE;
+
+-- Tablas a alterar
+-- p_fecha_pre_proy
+-- p_fecha_present_vol
+-- p_fecha_dup
+-- p_fecha_mitad_proy_estim
+-- p_fecha_mitad_proy_real
+-- p_fecha_cierre_proy_estim
+-- p_fecha_cierre_proy_real
+
+SET @S = CONCAT("ALTER TABLE `t_proyectos` CHANGE `p_fecha_pre_proy` `p_fecha_pre_proy` DATE NOT NULL DEFAULT '", @FE,"'");
+-- SELECT @S;
+PREPARE stmt FROM @S;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+SET @S = CONCAT("ALTER TABLE `t_proyectos` CHANGE `p_fecha_present_vol` `p_fecha_present_vol` DATE NOT NULL DEFAULT '", @FE,"'");
+-- SELECT @S;
+PREPARE stmt FROM @S;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+SET @S = CONCAT("ALTER TABLE `t_proyectos` CHANGE `p_fecha_dup` `p_fecha_dup` DATE NOT NULL DEFAULT '", @FE,"'");
+-- SELECT @S;
+PREPARE stmt FROM @S;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+SET @S = CONCAT("ALTER TABLE `t_proyectos` CHANGE `p_fecha_mitad_proy_estim` `p_fecha_mitad_proy_estim` DATE NOT NULL DEFAULT '", @FE,"'");
+-- SELECT @S;
+PREPARE stmt FROM @S;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+SET @S = CONCAT("ALTER TABLE `t_proyectos` CHANGE `p_fecha_mitad_proy_real` `p_fecha_mitad_proy_real` DATE NOT NULL DEFAULT '", @FE,"'");
+-- SELECT @S;
+PREPARE stmt FROM @S;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+SET @S = CONCAT("ALTER TABLE `t_proyectos` CHANGE `p_fecha_cierre_proy_estim` `p_fecha_cierre_proy_estim` DATE NOT NULL DEFAULT '", @FE,"'");
+-- SELECT @S;
+PREPARE stmt FROM @S;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+
+SET @S = CONCAT("ALTER TABLE `t_proyectos` CHANGE `p_fecha_cierre_proy_real` `p_fecha_cierre_proy_real` DATE NOT NULL DEFAULT '", @FE,"'");
+-- SELECT @S;
+PREPARE stmt FROM @S;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
