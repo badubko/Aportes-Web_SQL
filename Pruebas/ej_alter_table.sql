8139d0f7e8d013b5deb661bb95fba86ea45fe3c8
Basilio Dubko <badubko@gmail.com>
2019-03-21 15:42:08 -0300

    Mas ejemplos para guardar
    Signed-off-by: Basilio Dubko <badubko@gmail.com>

--- Pruebas/ej_alter_table.sql
+++ Pruebas/ej_alter_table.sql
@@ -4,3 +4,4 @@ alter table t_osc_contactos add  `id_truch` INT UNSIGNED NOT NULL AUTO_INCREMENT
 alter table t_osc_logs_estado add  `id_truch` INT UNSIGNED NOT NULL AUTO_INCREMENT AFTER last_update , ADD PRIMARY KEY (`id_truch`);
 
 ALTER TABLE `t_proyectos` CHANGE `p_fecha_cierre_proy_real` `p_fecha_cierre_proy_real` DATE NOT NULL DEFAULT '2100-01-01';
+ALTER TABLE t1 COMMENT = 'New table comment';
